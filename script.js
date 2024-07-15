36fd27ab6bf9c756ba96486a40db472eff33d120
sasidharan7373 <sasidharan07373@gmail.com>
2024-07-15 12:48:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+var sidenav = document.querySelector(".navbar")
+
+function showNavbar(){
+    sidenav.style.top= "0px"
+}
+
+function closeNavbar(){
+    sidenav.style.top= "-350px"
+}
+
+// slider in header
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("box1");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block"; 
+//   setTimeout(carousel, 2000); 
+// }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("box1");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); 
+  // Change image every 2 seconds
+}
+
+
+   // parallax///////////////
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add("show-items");
+    }else{
+      entry.target.classList.remove("show-items");
+    }
+  });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
